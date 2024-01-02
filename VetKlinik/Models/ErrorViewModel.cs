2321ee012916a7af305e2460cfee5f27e0e2f66d
Furkan Kümüş <furkankumus1905@gmail.com>
2024-01-03 02:38:35 +0300

    init code

---
+++ VetKlinik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VetKlinik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
