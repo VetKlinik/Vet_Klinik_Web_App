678fa43749137fa04a9b87f3f2211b1472cdb762
Furkan Kümüş <furkankumus1905@gmail.com>
2024-01-06 16:03:48 +0300

    View Component added

---
+++ VetKlinik/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace VetKlinik.Models
+{
+    public class About
+    {
+        public string AboutTxt { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+    }
+}
---
+++ VetKlinik/ViewComponents/AboutPageViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetKlinik.Models;
+
+namespace VetKlinik.ViewComponents
+{
+    public class AboutPageViewComponent : ViewComponent
+    {
+        //TODO View Component oluşturulması
+        public IViewComponentResult Invoke()
+        {
+            List<About> datas = new List<About>
+            {
+                new About{AboutTxt = "Bu bir about sayfası için oluşturulmuş hakkında yazısıdır", ContactEmail = "ferraridownshift@gmail.com", ContactPhone = "0542 542 42 42"},
+                new About{AboutTxt = "Bu bir about sayfası için oluşturulmuş ikinci bir hakkında yazısıdır", ContactEmail = "ferraridownshift@gmail.com", ContactPhone = "0542 542 42 42"}
+            };
+            return View(datas);
+        }
+    }
+}
