8ba03bcb4232c8c9607d17bfa8d9f89b28671882
Furkan Kümüş <furkankumus1905@gmail.com>
2024-01-11 20:01:55 +0300

    about n services added for client

---
+++ VetKlinik/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetKlinik.Services;
+
+namespace VetKlinik.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IGonderiService _service;
+        public AboutController(IGonderiService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var gonderiler = _service.GetGonderiler();
+
+            return View(gonderiler);
+        }
+    }
+}
---
+++ VetKlinik/Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetKlinik.Services;
+
+namespace VetKlinik.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly IHizmetlerService _hizmetlerContext;
+
+        public ServicesController(IHizmetlerService hizmetlerContext)
+        {
+            _hizmetlerContext = hizmetlerContext;
+        }
+
+        public IActionResult Index()
+        {
+            var hizmetler = _hizmetlerContext.GetHizmetler();
+
+            ViewBag.hizmetler = hizmetler;
+
+            return View(hizmetler);
+        }
+    }
+}
